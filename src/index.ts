0ccc32033a3805fe5bc375101dc120e8f979d277
Irez Abdullah <irezabdullah@rezweb.my.id>
2024-10-23 21:19:14 +0700

    feat(cors): add CORS configuration with custom allowed origins

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+import corsOptions from './config/corsConfig';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import bodyParser from 'body-parser';
@@ -7,12 +9,14 @@ dotenv.config();
 
 const app: Application = express();
 const port = 3000;
-connectDB();
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(bodyParser.json());
+connectDB();
 
-  // Routing
+// Routing
   app.use('/api/auth', authRoutes); //routing for login or register
-  
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
