13014b86bd418b1fd9649933be87fae8bbcf95d4
Irez Abdullah <irezabdullah@rezweb.my.id>
2024-10-26 11:16:39 +0700

    feat: rename user registration endpoint - to improve frontend clarity

--- src/routes/public-route.ts
+++ src/routes/public-route.ts
@@ -3,7 +3,7 @@ import {UserController} from "../controllers/user-controller";
 
 const router = express.Router();
 
-router.post('/users', UserController.register);
+router.post('/users/register', UserController.register);
 router.post('/users/login', UserController.login);
 
 export default router;
